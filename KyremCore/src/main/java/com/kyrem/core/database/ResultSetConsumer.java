ea6efc67143d701d77fe7f4bec55d94fce464738
ENDERGAMES096 <105583605+ENDERGAMES096@users.noreply.github.com>
2026-04-02 16:44:17 +0200

    Moved all the files to the right directory

--- KyremCore/src/main/java/com/kyrem/core/database/ResultSetConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.kyrem.core.database;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultSetConsumer<T> {
-    T handle(ResultSet rs) throws SQLException;
-}
